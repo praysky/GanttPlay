7b05882679f28994a957f3a3d3430b623825b6cc
praysky <pavelraysky@gmail.com>
2022-10-04 19:20:01 +0300

    id2

--- e2e/loginPage/loginTest/loginTest.spec.js
+++ e2e/loginPage/loginTest/loginTest.spec.js
@@ -15,20 +15,20 @@ import { faker } from '@faker-js/faker';
 test('Id 2: sign in', async ({ page }) => {
   const locator = new Locator(page)
   await page.goto('https://ganttpro.com/en/#/login');
+  await expect(page).toHaveScreenshot()
   await locator.inputEmail.fill(locator.email)
-  //await page.frameLocator('#frame').locator('#email').fill("playwright@test.test")
   await locator.inputPassword.fill(locator.passwordValue)
   
   //await page.screenshot({ path: 'screenshot1.png' });
   await locator.buttonLoginFormPopup.click()
   
   await expect(page).toHaveTitle(/GanttPRO/);
-  await expect(page).toHaveScreenshot()
+  
   
   
 
 });
-test('Id 1: sign up', async ({ page }) => {
+test.only('Id 1: sign up', async ({ page }) => {
   const locator = new Locator(page)
 await page.goto(locator.urlMainPage)
   await locator.buttonSignUpLoginPage.click()
@@ -39,7 +39,10 @@ await page.goto(locator.urlMainPage)
   await page.locator('//div[@class="welcome_main_buttons add_dropdown"]/select').selectOption("marketing"); //industry
   await page.locator('//div[@class="welcome_main_buttons"]/div[3]').click() //count users
   await page.locator('//div[@class="welcome_main_button h72 js-company-submit-btn loading-btn green"]').click() //button
-  await page.locator('//div[@class="webix_el_box"]/input[@placeholder="Please enter your first name ..."]') //firstname
+  await page.locator('//div[@class="webix_el_box"]/input[@placeholder="Please enter your first name ..."]').fill(faker.fake.bind.name) //firstname
+  await page.locator('//div[@view_id="formLastName"]//input') //lastname
+  await page.locator('//div[@view_id="welcomePasswordSection"]//input') // password
+  await page.locator('//div[@class="welcome_main_button h72 js-user-submit-btn loading-btn step_2 green"]') //button create account
   //await locator.selectUserIndustry
 });
 // });
